840d6d5b311fb14965ced5f26220d5f49ccc10c8
wangpan <wp01100381>
2019-11-03 09:39:57 +0800

    add new website

--- Source/Tailwind.Traders.Web/ClientApp/src/shared/header/header.js
+++ Source/Tailwind.Traders.Web/ClientApp/src/shared/header/header.js
@@ -113,7 +113,7 @@ class Header extends Component {
                             <Link className="main-nav__item" to="/list/diytools">
                                 {t('shared.header.diytools')}
                             </Link>
-                            <Link className="main-nav__item" to="../../../../assets/images/designer.jpg">
+                            <Link className="main-nav__item" to="https://1656988059.wixsite.com/garden">
                                 {t('shared.header.designerHome')}
                             </Link>
                             <div className="main-nav__actions">
